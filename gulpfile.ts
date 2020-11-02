3778a6b807cbdf820d94692b767629af0a43df02
Concision <contact@concision.me>
2020-11-01 20:32:18 -0800

    feat(gulp): implemented build script

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,10 @@
-import path from "path";
-import {Gulpclass, Task} from "gulpclass";
+import gulp from "gulp";
+import {Gulpclass, SequenceTask, Task} from "gulpclass";
 import typescript, {Project} from "gulp-typescript";
 import del from "del";
+import run from "gulp-run";
+import {resolve} from "path";
+import {readFileSync, writeFileSync} from "fs";
 
 @Gulpclass()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,19 +15,29 @@ class Gulpfile {
      * TypeScript runtime project configuration
      * @private
      */
-    private readonly project: Project = typescript.createProject(path.resolve(__dirname, "tsconfig.json"));
+    private readonly project: Project = typescript.createProject(resolve(__dirname, "tsconfig.json"));
 
     /**
      * Root source directory
      * @private
      */
-    private readonly root: string = path.resolve(__dirname, this.project.config.compilerOptions.rootDir);
+    private readonly root: string = resolve(__dirname, this.project.config.compilerOptions.rootDir);
     /**
      * Targeted output directory
      * @private
      */
-    private readonly target: string = path.resolve(__dirname, this.project.config.compilerOptions.outDir);
+    private readonly target: string = resolve(__dirname, this.project.config.compilerOptions.outDir);
 
+
+    // build
+
+    /**
+     * Build project
+     */
+    @SequenceTask("build")
+    public buildTask(): string[] {
+        return ["clean", "lint", "transpile", "includes", "package"];
+    }
 
     /**
      * Delete {@link target} directory
@@ -32,5 +45,71 @@ class Gulpfile {
     @Task("clean")
     public cleanTask(): Promise<unknown> {
         return del(this.target);
+    }
+
+    /**
+     * Lint source files
+     */
+    @Task("lint")
+    public lintTask(): void {
+        return run("npm run lint", {cwd: __dirname, verbosity: 3}).exec();
+    }
+
+    /**
+     * Transpile TypeScript project sources
+     */
+    @Task("transpile")
+    public transpileTask(): unknown {
+        const sources: string[] = [
+            // included files, if specified
+            ...(this.project.config.files ?? []),
+            // included file globs, if specified
+            ...(this.project.config.include ?? []),
+            // blacklist excluded globs, if specified; map to negated glob filter
+            ...(this.project.config.exclude?.map((pattern: string) => `!${pattern}`) ?? []),
+        ].filter(source => source != "gulpfile.ts");
+
+        return gulp.src(sources, {allowEmpty: true, base: this.root})
+            // transpile TypeScript sources
+            .pipe(this.project())
+            // write to target build directory
+            .pipe(gulp.dest(this.target));
+    }
+
+    /**
+     * Copy other files to distributed files
+     */
+    @Task("includes")
+    public includeTask(): unknown {
+        return Promise.all([
+            // add README.md and LICENSE
+            gulp.src(["README.md", "LICENSE"], {base: __dirname})
+                // write to target build directory
+                .pipe(gulp.dest(this.target)),
+            // add module typings
+            gulp.src(["**/typings/index.d.ts"], {base: resolve(this.root, "typings")})
+                // write to target build directory
+                .pipe(gulp.dest(this.target)),
+        ]);
+    }
+
+    /**
+     * Copy stripped package.json to distributed files
+     */
+    @Task("package")
+    public async packageTask(): Promise<void> {
+        // read package.json
+        const packageJson = JSON.parse(readFileSync(resolve(__dirname, "package.json"), "utf8"));
+
+        // delete unnecessary tags
+        delete packageJson["type"];
+        delete packageJson["scripts"];
+        delete packageJson["devDependencies"];
+
+        // write package.json
+        writeFileSync(
+            resolve(this.target, "package.json"),
+            JSON.stringify(packageJson, null, "  "),
+        );
     }
 }
