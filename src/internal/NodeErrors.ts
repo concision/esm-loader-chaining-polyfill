b6e751bf45bb34ae6bea1b84970cfbe99cc1a175
Concision <contact@concision.me>
2020-11-05 12:20:59 -0800

    fix: improved typings for Node errors

--- src/internal/NodeErrors.ts
+++ src/internal/NodeErrors.ts
@@ -1,7 +1,14 @@
+export interface NodeError<E extends string> {
+    code: E;
+}
+
 /**
  * Source: https://github.com/nodejs/node/blob/9c1c17a84168c5742084c34ac3395ca38bc182eb/lib/internal/errors.js#L1166-L1175
  */
-export class ERR_INVALID_RETURN_VALUE extends TypeError {
+export class ERR_INVALID_RETURN_VALUE extends TypeError implements NodeError<"ERR_INVALID_RETURN_VALUE"> {
+    public readonly code: "ERR_INVALID_RETURN_VALUE";
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any
     constructor(input: string, name: string, value: any) {
         let type;
         if (value?.constructor?.name) {
@@ -11,6 +18,6 @@ export class ERR_INVALID_RETURN_VALUE extends TypeError {
         }
         super(`Expected ${input} to be returned from the "${name}" function but got ${type}.`);
 
-        (<any>this).code = "ERR_INVALID_RETURN_VALUE";
+        this.code = "ERR_INVALID_RETURN_VALUE";
     }
 }
