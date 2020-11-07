72f426696b4739ae29594a6a1cb020bc8235e118
Concision <contact@concision.me>
2020-11-07 15:02:06 -0800

    feat: updated no-tla ESM loader hook warning

--- src/no-tla.ts
+++ src/no-tla.ts
@@ -1,7 +1,12 @@
 import {EsmLoaderHook} from "./typings";
 import {createEsmLoader} from "./loader/EsmChainingLoaderBuilder.js";
 
-process.emitWarning("esm-loader-chaining: --experimental-top-level-await or --harmony-top-level-await must be specified to properly support the getGlobalPreloadCode hook");
+process.emitWarning(
+    "esm-loader-chaining/no-tla: --experimental-top-level-await or --harmony-top-level-await must be specified" +
+    " with the default 'esm-loader-chaining' ESM chaining loader to fully support the getGlobalPreloadCode hook." +
+    " Otherwise, only CommonJS ESM loaders will have their getGlobalPreloadCode hook supported.",
+);
+
 export const {
     getGlobalPreloadCode,
     resolve,
