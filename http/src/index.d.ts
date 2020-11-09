c5f718502ccc920af4ac7fd9bfdae8cddd804632
Concision <contact@concision.me>
2020-11-08 16:48:55 -0800

    feat(http): added module typings

---
+++ http/src/index.d.ts
@@ -0,0 +1,5 @@
+import {ModuleFormatHook, ResolveHook, SourceHook} from "@esm-loaders/types";
+
+export const resolve: ResolveHook;
+export const getFormat: ModuleFormatHook;
+export const getSource: SourceHook;
